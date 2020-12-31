c775aa013eabf6231f68473a57ad8c9f3b754a68
Raymond Truong <raylintruong@gmail.com>
2020-12-30 20:37:32 -0500

    Selector responsiveness

--- src/pages/CheckoutPage/ItemSelector/ItemCard.vue
+++ src/pages/CheckoutPage/ItemSelector/ItemCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card max-width="256" height="124" tile outlined class="mt-2">
+    <v-card max-width="256" height="160" tile outlined class="mt-2">
         <v-card-text class="black--text title">
             {{ getName }}
         </v-card-text>
--- src/pages/CheckoutPage/ItemSelector/index.vue
+++ src/pages/CheckoutPage/ItemSelector/index.vue
@@ -3,13 +3,24 @@
         <v-data-iterator
             v-if="this.items.length > 0"
             :items="items"
-            :items-per-page="itemsPerPage"
+            :items-per-page="ipp"
             :page.sync="page"
             hide-default-footer
             class="d-flex flex-column mh-100 overflow-hidden"
         >
             <template v-slot:header>
                 <v-toolbar flat dark color="grey darken-3" class="shrink mb-1">
+                    <v-btn
+                        class="ml-2"
+                        outlined
+                        v-for="category in categories"
+                        :key="category.name"
+                        large
+                    >
+                        <div class="title">
+                            {{ category.name }}
+                        </div>
+                    </v-btn>
                     <v-spacer />
                     <span
                         class="mr-4
@@ -18,6 +29,7 @@
                         Page {{ page }} of {{ numberOfPages }}
                     </span>
                     <v-btn
+                        large
                         dark
                         outlined
                         color="white darken-3"
@@ -27,6 +39,7 @@
                         <v-icon>mdi-chevron-left</v-icon>
                     </v-btn>
                     <v-btn
+                        large
                         dark
                         outlined
                         color="white darken-3"
@@ -47,7 +60,7 @@
                         <v-col
                             v-for="item in props.items"
                             :key="item.name"
-                            :cols="12 / rowsPerPage"
+                            :cols="12 / itemsPerRow"
                             class="py-2"
                         >
                             <ItemCard :name="item.name" />
@@ -85,25 +98,76 @@ export default {
         items: Array,
     },
 
-    mounted() {
-        this.numberOfPages = Math.ceil(this.items.length / this.itemsPerPage);
+    created() {
+        window.addEventListener("resize", () => {
+            this.calcRowsPerPage();
+        });
     },
 
     data: function() {
         return {
+            rpp: 4,
             page: 1,
-            rowsPerPage: 4,
-            itemsPerPage: 16,
-            numberOfPages: 0,
+            busy: false,
+            categories: [
+                {
+                    name: "✋ Manicure",
+                },
+                {
+                    name: "🦶 Pedicure",
+                },
+                {
+                    name: "🧒 Kids",
+                },
+                {
+                    name: "❔ Other",
+                },
+            ],
         };
     },
-    computed: {},
+    computed: {
+        numberOfPages() {
+            return Math.ceil(this.items.length / this.ipp);
+        },
+        rowsPerPage() {
+            return this.rpp;
+        },
+        itemsPerRow() {
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs":
+                    return 1;
+                case "sm":
+                    return 2;
+                case "md":
+                    return 3;
+                case "lg":
+                    return 4;
+                case "xl":
+                    return 6;
+                default:
+                    return 3;
+            }
+        },
+        ipp() {
+            return Math.ceil(this.rowsPerPage * this.itemsPerRow);
+        },
+    },
     methods: {
         nextPage() {
             if (this.page + 1 <= this.numberOfPages) this.page += 1;
         },
         formerPage() {
             if (this.page - 1 >= 1) this.page -= 1;
+        },
+        calcRowsPerPage() {
+            const container = document.getElementById("container");
+            if (container) {
+                const containerHeight = parseInt(container.clientHeight, 0);
+                const minItemHeight = 160;
+                this.rpp = Math.floor(containerHeight / minItemHeight);
+            } else {
+                this.rpp = 4;
+            }
         },
     },
 };
